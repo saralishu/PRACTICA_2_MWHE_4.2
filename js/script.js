320b27e3e44a7d3fe14372463c916fc95ba26e05
Maca-web <macarenamgva@gmail.com>
2024-12-20 17:02:17 +0100

    menu

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("Hello world!");
+
+// Abrir y cerrar menu
+function openMenu() {
+    "use strict";
+    document.getElementById("menu").style.right = "0";
+}
+
+function closeMenu() {
+    "use strict";
+    document.getElementById("menu").style.right = "-100%";
+}
